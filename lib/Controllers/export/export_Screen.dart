22526280436e388dc4cc632ae458dd952965bb9b
unknown <adii.developer277@gmail.com>
2022-08-16 11:58:12 +0500

    second commit

---
+++ lib/Controllers/export/export_Screen.dart
@@ -0,0 +1,4 @@
+export  'package:flutter/material.dart';
+
+//views
+export  'package:book_grocer/Views/splash/splash_screen.dart';
---
+++ lib/Views/splash/splash_screen.dart
@@ -0,0 +1,11 @@
+
+import 'package:book_grocer/Controllers/export/export_Screen.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
